d22442d47a18e394de9d10f3c2077e65a73f1450
Ngô Tuấn Anh <ngotuananh2101@gmail.com>
2021-11-12 21:27:31 +0700

    fix size admin product couumns

--- AdminProduct.Designer.cs
+++ AdminProduct.Designer.cs
@@ -68,11 +68,13 @@ namespace MilkTeaManagement
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 11);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.dataGridView1.RowTemplate.Height = 29;
             this.dataGridView1.Size = new System.Drawing.Size(1039, 993);
             this.dataGridView1.TabIndex = 4;
