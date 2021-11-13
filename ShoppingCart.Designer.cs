dfb79f4b440214b066e5de5be6928f2fc3636f0a
K.Long <76844316+longcodebad@users.noreply.github.com>
2021-11-14 01:24:37 +0700

    Add files via upload
    Se co anh

---
+++ ShoppingCart.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace ShoppingCartNew
+{
+    partial class ShoppingCart
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboCategory = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Employee = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(634, 637);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(950, 642);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1048, 642);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1151, 703);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(406, 45);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // comboCategory
+            // 
+            this.comboCategory.FormattingEnabled = true;
+            this.comboCategory.Location = new System.Drawing.Point(22, 45);
+            this.comboCategory.Name = "comboCategory";
+            this.comboCategory.Size = new System.Drawing.Size(151, 28);
+            this.comboCategory.TabIndex = 5;
+            this.comboCategory.SelectedIndexChanged += new System.EventHandler(this.comboCategory_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(747, 95);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(635, 526);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(677, 341);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "=>";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(973, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(230, 27);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Employee
+            // 
+            this.Employee.AutoSize = true;
+            this.Employee.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Employee.Location = new System.Drawing.Point(848, 49);
+            this.Employee.Name = "Employee";
+            this.Employee.Size = new System.Drawing.Size(103, 25);
+            this.Employee.TabIndex = 9;
+            this.Employee.Text = "Employee :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(690, 707);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // ShoppingCart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1422, 759);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Employee);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.comboCategory);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ShoppingCart";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.ShoppingCart_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboCategory;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Employee;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ShoppingCart.cs
@@ -0,0 +1,188 @@
+﻿using DemoDataAccess;
+using ShoppingCartNew.Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingCartNew
+{
+    public partial class ShoppingCart : Form
+    {
+        CategoryBusiness catList = new CategoryBusiness();
+        ProductBusiness proList = new ProductBusiness();
+        public ShoppingCart()
+        {
+            InitializeComponent();
+           
+        }
+
+        List<UserControl1> ProductControls = new List<UserControl1>();
+        private decimal CalculateFreight()
+        {
+            decimal sum = 0;
+            foreach (UserControl1 uc in ProductControls)
+                sum += uc.ProductPrice * uc.Quantity;
+            return sum;
+        }
+
+        //du lieu thay doi
+        private void Product_DataChange(object sender, EventArgs e)
+        {
+            label2.Text = CalculateFreight().ToString();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //in bill
+        }
+
+        //moi lan thay doi thi chon cach load
+        private void comboCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {//comboCategory.SelectedValue
+            //int i = Convert.ToInt32(comboCategory.SelectedValue.GetHashCode();
+            if (comboCategory.SelectedItem == null) 
+            { 
+               
+            }
+            else
+            { 
+                var product = new Product()
+                {
+                    CategoryID = Convert.ToInt32(comboCategory.SelectedValue.GetHashCode())
+                };
+                List<Product> productscat = proList.GetProductsWithCatID(product);
+                dataGridView1.DataSource = productscat;
+                this.dataGridView1.Columns["ProductID"].Visible = false;
+                this.dataGridView1.Columns["CategoryID"].Visible = false;
+            }
+        }
+
+        //dung de load lai form ma ko can thhay doi index
+        private void reloadEmployee()
+        {
+            // comboCategory.SelectedIndex = 0;
+            
+
+
+        }
+
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            //lay tu datagridview
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (Convert.ToBoolean(row.Cells[0].Value))
+                {
+
+                    for (int i = 0; i <= dataGridView1.Rows.Count;i++)
+                    {
+                        //Product p; UserControl1 MyControl = new UserControl1(p.ProductID, p.ProductName, p.Price, 1);
+                        UserControl1 MyControl = new UserControl1(row.Cells[1].Value.ToString(), row.Cells[2].Value.ToString(), Convert.ToDecimal(row.Cells[3].Value), 1);
+                        MyControl.PropertyChange += Product_DataChange;
+                        if (!ProductControls.Contains(MyControl))
+                        {
+                            ProductControls.Add(MyControl);
+                            flowLayoutPanel1.Controls.Add(MyControl);
+                            //break de khong vuong vao vonng loop cua 3 san pham
+                            break;
+
+                        }
+                        else
+                        {
+                            ProductControls[ProductControls.IndexOf(MyControl)].Quantity += 1;
+                            //tuong tu
+                            break;
+                        }
+                    }
+            }
+           
+
+            }
+            Product_DataChange(null, null);
+        }
+        private void NormalImage(object sender, EventArgs e)
+        {
+
+            foreach (DataGridViewImageColumn column in
+                dataGridView1.Columns)
+            {
+                column.ImageLayout = DataGridViewImageCellLayout.Normal;
+                column.Description = "Normal";
+            }
+        }
+        private void ShoppingCart_Load(object sender, EventArgs e)
+        {
+            DataGridViewCheckBoxColumn cbColumn = new DataGridViewCheckBoxColumn();
+            cbColumn.HeaderText = "Check";
+            cbColumn.Name = "cbColumn";
+            dataGridView1.Columns.Add(cbColumn);
+            DataGridViewImageColumn dgvi = new DataGridViewImageColumn();
+
+            comboCategory.DataSource = new CategoryBusiness().GetCategories();
+           
+            comboCategory.SelectedItem = null;
+            comboCategory.SelectedText = "--select--";
+
+
+
+            //this.dataGridView1.Columns["Image"].
+
+            //dataGridView1.AutoGenerateColumns = false;
+
+
+
+
+            List<Product> products = proList.GetProducts();
+            dataGridView1.DataSource = products;
+            
+            
+
+            DataGridViewImageColumn img = new DataGridViewImageColumn();
+            img.HeaderText = "Image2";
+            img.Name = "img";
+            
+           
+            img.ImageLayout = DataGridViewImageCellLayout.Normal;
+            for(int i=0; i < dataGridView1.Rows.Count; i++)
+            {
+                //string zz = dataGridView1.Rows[i].Cells[5].Value.ToString();
+                //Image image = Image.FromFile(zz);
+                //img.Image = image;
+                //break;
+                    
+                    string zz = dataGridView1.Rows[i].Cells[5].Value.ToString();
+                    Image image2 = Image.FromFile(@zz);
+                    img.Image = image2;
+                   // dataGridView1.Rows.Add();
+                   // dataGridView1["Image",i].Value = image2;
+                    //dataGridView1.Rows[i].Cells[6].Value = image2;
+                    //dataGridView1.Rows.Add( image2);
+                //neu lam else
+                //Image image = null;
+                //dataGridView1.Rows[i].Cells[6].Value = image;
+
+            }
+                dataGridView1.Columns.Add(img);
+
+
+
+
+
+            //combo box
+            //comboCategory.DropDownStyle = ComboBoxStyle.DropDownList;
+            //comboCategory.SelectedIndex = 0;
+
+            this.dataGridView1.Columns["productid"].Visible = false;
+            this.dataGridView1.Columns["categoryid"].Visible = false;
+
+            flowLayoutPanel1.AutoScroll = true;
+        }
+    }
+}
